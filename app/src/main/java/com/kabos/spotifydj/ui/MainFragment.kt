e3f449fc40a00e3404ab7ace64f320e5ca98f40c
taratara10 <shuhei.knhs@gmail.com>
2021-07-10 16:27:49 +0900

    Hiltの依存関係を修正

---
+++ app/src/main/java/com/kabos/spotifydj/ui/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.kabos.spotifydj.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.kabos.spotifydj.databinding.FragmentMainBinding
+import com.kabos.spotifydj.viewModel.UserViewModel
+
+class MainFragment: Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    private val viewModel: UserViewModel by activityViewModels()
+    private lateinit var accessToken: String
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        accessToken = requireActivity().getSharedPreferences("SPOTIFY", 0)
+            .getString("token", "No token").toString()
+
+        binding.apply {
+            button2.setOnClickListener {
+                viewModel.getUser(accessToken)
+            }
+        }
+    }
+}
